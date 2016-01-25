7b52cc64edec497ed6e774c2de7432773458eecf
Dominic Oram <dominic.oram@stfc.ac.uk>
2016-01-25 14:53:03 +0000

    Added EPICS_KIT_ROOT

--- run_tests.bat
+++ run_tests.bat
@@ -1,11 +1,11 @@
 set PYTHON_EXE=c:\Python27\python.exe
+set EPICS_KIT_ROOT=C:\Instrument\Apps\EPICS
 set MYPVPREFIX=TEST
-
  
 REM Run the blockserver tests
 cd .\BlockServer
 %PYTHON_EXE% run_tests.py -o ..\..\..\test-reports
 
 REM Run the databaseServer tests
-cd .\DatabaseServer
+cd ..\DatabaseServer
 %PYTHON_EXE% run_tests.py -o ..\..\..\test-reports
