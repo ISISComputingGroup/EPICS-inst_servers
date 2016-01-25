3148d3b0a9508cd2279c7564debea02ef8f49e21
Dominic Oram <dominic.oram@stfc.ac.uk>
2016-01-25 13:47:45 +0000

    Added batch for running tests

---
+++ run_tests.bat
@@ -0,0 +1,7 @@
+REM Run the blockserver tests
+cd \BlockServer
+%PYTHON_EXE% run_tests.py -o ..\..\..\test-reports
+
+REM Run the databaseServer tests
+cd \DatabaseServer
+%PYTHON_EXE% run_tests.py -o ..\..\..\test-reports
