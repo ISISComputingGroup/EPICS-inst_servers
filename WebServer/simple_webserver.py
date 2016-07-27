88d2369f39a1a53270d00fdebdae9ac7e2241e9c
Sean Marshall <sean-marshall@hotmail.co.uk>
2016-07-27 14:57:30 +0100

    Web Server to change when reloaded

--- WebServer/simple_webserver.py
+++ WebServer/simple_webserver.py
@@ -1,15 +1,23 @@
 from BaseHTTPServer import BaseHTTPRequestHandler,HTTPServer
-HOST, PORT = '', 8888
-
+import time
+HOST, PORT = '', 8800
+start = time.time()
 class myHandler(BaseHTTPRequestHandler):
 
     def do_GET(self):
         self.send_response(200)
         self.send_header('Content-type','text/html')
         self.end_headers()
-        self.wfile.write("Hello World!")
+        now = time.time()
+        difference = now-start
+        if difference <= 3:
+            self.wfile.write("Hello (1)!")
+        elif difference <= 6:
+            self.wfile.write("Hello (2)!")
+        else:
+            self.wfile.write("Hello (3)!")
 
 
-server=HTTPServer(('',PORT), myHandler)
+server = HTTPServer(('',PORT), myHandler)
 print "Serving HTTP on port %s ..." % PORT
 server.serve_forever()
