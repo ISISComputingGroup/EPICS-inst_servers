181a4a47eca752642ceb91e6d77e5cd41b482a44
Dominic Oram <dominic.oram@stfc.ac.uk>
2016-07-27 16:18:31 +0100

    Added global to the counter

--- WebServer/simple_webserver.py
+++ WebServer/simple_webserver.py
@@ -1,17 +1,18 @@
 from BaseHTTPServer import BaseHTTPRequestHandler,HTTPServer
 import time
+recursions = 0
 
 HOST, PORT = '', 8800
 class myHandler(BaseHTTPRequestHandler):
-    recursions = 0
 
     def do_GET(self):
         self.send_response(200)
         self.send_header('Content-type','text/html')
         self.end_headers()
         self.wfile.write('<html><head><meta http-equiv="refresh" content="10"></head>')
-        self.recursions = self.recursions + 1
-        self.wfile.write("Hello (" + str(int(self.recursions)) + ")!")
+        global recursions
+        recursions = recursions + 1
+        self.wfile.write("Hello (" + str(int(recursions)) + ")!")
 
 
 server = HTTPServer(('',PORT), myHandler)
