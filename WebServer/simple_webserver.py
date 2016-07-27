2e073625ffde7143b5d4ad1779be1a9bd551b389
Sean Marshall <sean-marshall@hotmail.co.uk>
2016-07-27 16:10:21 +0100

    Refreshing but not counting

--- WebServer/simple_webserver.py
+++ WebServer/simple_webserver.py
@@ -1,21 +1,17 @@
 from BaseHTTPServer import BaseHTTPRequestHandler,HTTPServer
 import time
+
 HOST, PORT = '', 8800
-start = time.time()
 class myHandler(BaseHTTPRequestHandler):
+    recursions = 0
 
     def do_GET(self):
         self.send_response(200)
         self.send_header('Content-type','text/html')
         self.end_headers()
-        now = time.time()
-        difference = now-start
-        if difference <= 3:
-            self.wfile.write("Hello (1)!")
-        elif difference <= 6:
-            self.wfile.write("Hello (2)!")
-        else:
-            self.wfile.write("Hello (3)!")
+        self.wfile.write('<html><head><meta http-equiv="refresh" content="10"></head>')
+        self.recursions = self.recursions + 1
+        self.wfile.write("Hello (" + str(int(self.recursions)) + ")!")
 
 
 server = HTTPServer(('',PORT), myHandler)
