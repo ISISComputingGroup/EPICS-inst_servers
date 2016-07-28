c85d827c9a7a92177f8357b3d5b030940ad6c4f0
Sean Marshall <sean-marshall@hotmail.co.uk>
2016-07-28 09:45:15 +0100

    Refreshes and counts accurately

--- WebServer/simple_webserver.py
+++ WebServer/simple_webserver.py
@@ -1,17 +1,18 @@
 from BaseHTTPServer import BaseHTTPRequestHandler,HTTPServer
-import time
 recursions = 0
 
 HOST, PORT = '', 8800
 class myHandler(BaseHTTPRequestHandler):
 
     def do_GET(self):
+        if self.path == "/favicon.ico":
+            return
         self.send_response(200)
         self.send_header('Content-type','text/html')
         self.end_headers()
         self.wfile.write('<html><head><meta http-equiv="refresh" content="10"></head>')
         global recursions
-        recursions = recursions + 1
+        recursions += 1
         self.wfile.write("Hello (" + str(int(recursions)) + ")!")
 
 
