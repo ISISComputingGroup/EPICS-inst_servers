346a03c46ab739a471769744ee4e5c35c1597dfb
Jack Harper <jack.harper@stfc.ac.uk>
2020-11-18 15:53:38 +0000

    whoops, wrong place to specify encoding

--- server_common/test_modules/test_utilities.py
+++ server_common/test_modules/test_utilities.py
@@ -15,5 +15,5 @@ class TestUtilities(unittest.TestCase):
     def test_GIVEN_string_WHEN_compressing_and_hexing_THEN_output_is_compressed_and_hexed_correctly(self):
         test = "test"
         value = compress_and_hex(test)
-        expected_value = binascii.hexlify(bytes(test, encoding="utf-8"))
+        expected_value = binascii.hexlify(bytes(test))
         assert_that(value, is_(expected_value))
