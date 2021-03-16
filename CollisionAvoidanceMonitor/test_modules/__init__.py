5fad4ceec8fa6fb04dfd55f20e05e0cb9cf133aa
Dominic Oram <dominic.oram@stfc.ac.uk>
2021-03-16 09:24:19 +0000

    Minor review for CAM

--- CollisionAvoidanceMonitor/test_modules/__init__.py
+++ CollisionAvoidanceMonitor/test_modules/__init__.py
@@ -1,5 +1,4 @@
 import os
-import six
 import unittest
 
 
@@ -8,9 +7,9 @@ def load_tests(loader, standard_tests, pattern):
     This function is needed by the load_tests protocol described at
     https://docs.python.org/3/library/unittest.html#load-tests-protocol
 
-    The tests in this module are only added under Python 2 and don't run under linux.
+    The tests in this module are only added under Windows.
     """
-    if six.PY3 and os.name == "nt":
+    if os.name == "nt":
         standard_tests.addTests(loader.discover(os.path.dirname(__file__), pattern=pattern))
         return standard_tests
     else:
