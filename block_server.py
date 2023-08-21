adde72895325c7fbfd694a944922b5a88e266302
Isaac Hill <isaac.hill@stfc.ac.uk>
2023-08-21 14:36:55 +0100

    added a little comment to say hi

--- block_server.py
+++ block_server.py
@@ -19,7 +19,7 @@ import json
 import os
 import sys
 import traceback
-
+# hi
 from server_common.channel_access import verify_manager_mode, ManagerModeRequiredException
 
 sys.path.insert(0, os.path.abspath(os.path.dirname(__file__)))
