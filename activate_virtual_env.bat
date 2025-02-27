40fc00249010f9a7e035131f287b8831b5da269b
Jack Harper <jack.harper@stfc.ac.uk>
2025-02-27 14:20:32 +0000

    add create/activate venv scripts, use in _cmds

---
+++ activate_virtual_env.bat
@@ -0,0 +1,3 @@
+call "%~dp0.venv\Scripts\activate.bat"
+set PYTHON3="%~dp0.venv\Scripts\python.exe"
+set PYTHON3W="%~dp0.venv\Scripts\pythonw.exe"
--- start_archiver_access_cmd.bat
+++ start_archiver_access_cmd.bat
@@ -8,6 +8,8 @@ call %MYDIRBLOCK%..\..\..\config_env_base.bat
 set EPICS_CAS_INTF_ADDR_LIST=127.0.0.1
 set EPICS_CAS_BEACON_ADDR_LIST=127.255.255.255
 
+call %MYDIRBLOCK%activate_virtual_env.bat
+
 set PYTHONUNBUFFERED=TRUE
 
 %PYTHON3W% %MYDIRBLOCK%ArchiverAccess\archiver_access.py
