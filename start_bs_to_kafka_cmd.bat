ece0bf4475e97a38ca45349521a10d21cd9d920e
Jack Harper <jack.harper@stfc.ac.uk>
2025-07-28 11:28:17 +0100

    if on HIFI use supermusr redpanda cluster, else use livedata

--- start_bs_to_kafka_cmd.bat
+++ start_bs_to_kafka_cmd.bat
@@ -9,5 +9,13 @@ set EPICS_CAS_INTF_ADDR_LIST=127.0.0.1
 set EPICS_CAS_BEACON_ADDR_LIST=127.255.255.255
 
 set PYTHONUNBUFFERED=TRUE
+
+
+if "%INSTRUMENT%" == "NDXHIFI" (
+    set "BROKER=130.246.55.29:9092"
+) else (
+    set "BROKER=livedata.isis.cclrc.ac.uk:31092"
+)
+
 @echo %DATE% %TIME% starting BS to Kafka 
-%PYTHON3W% %MYDIRCD%\BlockServerToKafka\main.py -d %INSTRUMENT%_sampleEnv -c forwarder_config -b livedata.isis.cclrc.ac.uk:31092 -p %MYPVPREFIX%
+%PYTHON3W% %MYDIRCD%\BlockServerToKafka\main.py -d %INSTRUMENT%_sampleEnv -c %INSTRUMENT%_forwarderConfig -b %BROKER% -p %MYPVPREFIX%
