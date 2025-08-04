ed3fdedc79a107628c661de2a44aaaeaf1f22c66
Tom Willemsen <tom.willemsen@stfc.ac.uk>
2025-08-04 13:54:07 +0100

    Forward inst PVs

---
+++ BlockServerToKafka/inst_pvs.py
@@ -0,0 +1,36 @@
+import threading
+
+from genie_python.mysql_abstraction_layer import SQLAbstraction
+from server_common.utilities import print_and_log
+
+from BlockServerToKafka.kafka_producer import ProducerWrapper
+
+
+class InstPVs(object):
+    def __init__(self, producer: ProducerWrapper) -> None:
+        self._pvs = []
+        self._sql = SQLAbstraction(
+            dbid="iocdb", user="report", password="$report", host="localhost"
+        )
+        self.producer = producer
+
+    def schedule(self) -> None:
+        def action() -> None:
+            self._work()
+            self.schedule()
+
+        job = threading.Timer(30.0, action)
+        job.start()
+
+    def _work(self) -> None:
+        pvs = self._sql.query('SELECT pvname FROM iocdb.pvinfo WHERE infoname="archive";')
+        if pvs is None:
+            return
+
+        pvs = [pv[0] for pv in pvs]
+
+        if set(self._pvs) != set(pvs):
+            print_and_log(f"Inst configuration changed to: {pvs}")
+            self.producer.remove_config(self._pvs)
+            self.producer.add_config(pvs)
+            self._pvs = pvs
