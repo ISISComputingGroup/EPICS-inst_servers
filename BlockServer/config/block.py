5398728cd65592af428f8cdac159f4255263d368
vux62295 <vux62295@NDW2633.isis.cclrc.ac.uk>
2026-07-27 14:20:19 +0100

    More ruff fixes

--- BlockServer/config/block.py
+++ BlockServer/config/block.py
@@ -14,7 +14,7 @@
 # https://www.eclipse.org/org/documents/epl-v10.php or
 # http://opensource.org/licenses/eclipse-1.0.php
 
-from typing import TypedDict, Union
+from typing import TypedDict
 
 from server_common.helpers import PVPREFIX_MACRO
 
